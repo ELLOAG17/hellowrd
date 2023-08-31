bf09fcb8e5016705b7078269e4a65b196544627d
ELLOAG16 <lopezelian883@gmail.com>
2023-08-31 07:31:56 -0700

    Elian carpeta

---
+++ Elian/Docente.cs
@@ -0,0 +1,11 @@
+namespace HolaMundo.Elian;
+
+//PascalCase
+class Docente
+{
+    public int NumeroDeEmpleado { get; set;}
+
+    public string? Nombres { get; set;}
+
+    public string? Apellidos {get; set;}
+}
